cf06547f34b26e2a571707093a87413802a9a6bd
Hardik Khatri <hardikkhatri1312@gmail.com>
2022-05-16 13:38:18 +0530

    Added signup functionality

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from .models import Profile
 
 # Create your views here.
 
@@ -32,7 +33,9 @@ def signup(request):
 
                 # Create a Profile object for the new user
                 user_model = User.objects.get(username=username)
-                new_Profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
         else:
             messages.info(request, 'Passowrd is not matching.')
             return redirect('signup')
