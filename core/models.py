1ddb6bf96aacb5a03197dab8749f68fec2bbea28
Hardik Khatri <hardikkhatri1312@gmail.com>
2022-06-14 17:19:27 +0530

    Added functionality to follow and unfollow

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth import get_user_model
 import uuid
@@ -33,3 +34,10 @@ class LikePost(models.Model):
 
     def ___str___(self):
         return self.username
+
+class FollowersCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+
+    def ___str___(self):
+        return self.user
